3457cca2b92c93b448c57fe416640e1b3ae58238
Nadine Coelho <nadinecoelho9@gmail.com>
2021-02-03 12:49:43 -0300

    create js file to animation with gsap

---
+++ loaderTreeDots/main.js
@@ -0,0 +1,18 @@
+let elem = document.querySelectorAll(".square-ball");
+
+TweenMax.to(elem, 1, {
+  scale: 0.6,
+  borderRadius: "50%",
+  rotation: 360,
+  repeat: -1,
+});
+
+//////////*Sintaxe de um tween*/////////
+// Recebe três parâmetros.
+// Primeiro é o elemento que queremos animar
+// segundo é duração da animação
+// terceiro é um objeto contendo as propriedades do elemento que queremos animar
+
+// from - Anima o elemento da prop (tween) até as prop atuais (css)
+// to - Anima o elemento da prop atual (css) até as props passadas na prop (tween)
+// fromTo - Anima o elemento das props passadas como from até as props passadas como to
