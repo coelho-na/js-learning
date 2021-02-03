1ffe97d8efabae594aaaabcf6b6c02c33530157f
Nadine Coelho <nadinecoelho9@gmail.com>
2021-02-03 12:55:33 -0300

    finish animation

--- loaderTreeDots/main.js
+++ loaderTreeDots/main.js
@@ -5,6 +5,8 @@ TweenMax.to(elem, 1, {
   borderRadius: "50%",
   rotation: 360,
   repeat: -1,
+  repeatDelay: 0.5,
+  yoyo: true,
 });
 
 //////////*Sintaxe de um tween*/////////
@@ -16,3 +18,4 @@ TweenMax.to(elem, 1, {
 // from - Anima o elemento da prop (tween) até as prop atuais (css)
 // to - Anima o elemento da prop atual (css) até as props passadas na prop (tween)
 // fromTo - Anima o elemento das props passadas como from até as props passadas como to
+// propriedade 'yoyo:true' diz a animação que ao terminar ela volte fazendo a mesma coisa
