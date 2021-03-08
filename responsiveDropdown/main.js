cc030a2153c2414ad79a574438b9c47f88f44463
Nadine Coelho <nadinecoelho9@gmail.com>
2021-03-07 22:22:57 -0300

    finish html css and js files

---
+++ responsiveDropdown/main.js
@@ -0,0 +1,6 @@
+const navigation = document.querySelector(".nav");
+
+document.querySelector(".toggle").onclick = function () {
+  this.classList.toggle("active");
+  navigation.classList.toggle("active");
+};
