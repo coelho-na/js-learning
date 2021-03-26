2d889b2c4f1854aff3f50ebb2bc62c679066b84b
Nadine Coelho <nadinecoelho9@gmail.com>
2021-03-26 14:00:33 -0300

    finish style and html

---
+++ automaticShowPopUp/main.js
@@ -0,0 +1,14 @@
+// Automatic show popup after 3 seconds of page load
+
+const popup = document.querySelector(".popupBox");
+const close = document.querySelector(".close");
+
+window.onload = function () {
+  setTimeout(() => {
+    popup.style.display = "block";
+  }, 3000);
+};
+
+close.addEventListener("click", () => {
+  popup.style.display = "none";
+});
