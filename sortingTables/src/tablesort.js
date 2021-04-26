9ed29c5beec61f64b4b2a12fb5979a2fcec81a2f
Nadine Coelho <nadinecoelho9@gmail.com>
2021-04-26 18:31:24 -0300

    finish html and css

---
+++ sortingTables/src/tablesort.js
@@ -0,0 +1,42 @@
+let sortDirection = false;
+let personData = [
+  { name: "Dylan", age: 31 },
+  { name: "Vitória", age: 30 },
+  { name: "Israel", age: 29 },
+  { name: "Nayren", age: 14 },
+  { name: "Dollan", age: 25 },
+];
+
+window.onload = () => {
+  loadTableData(personData);
+};
+
+function loadTableData(personData) {
+  const tableBody = document.querySelector(".table__data");
+  let dataHtml = "";
+  for (let person of personData) {
+    dataHtml += `<tr><td>${person.name}</td><td>${person.age}</td></tr>`;
+  }
+  tableBody.innerHTML = dataHtml;
+}
+
+function sortColumn(columnName) {
+  const dataType = typeof personData[0][columnName];
+
+  sortDirection = !sortDirection;
+
+  switch (dataType) {
+    case "number":
+      sortNumberColumn(sortDirection, columnName);
+      break;
+  }
+  loadTableData(personData);
+}
+
+function sortNumberColumn(sort, columnName) {
+  personData = personData.sort((p1, p2) => {
+    return sort
+      ? p1[columnName] - p2[columnName]
+      : p2[columnName] - p1[columnName];
+  });
+}
